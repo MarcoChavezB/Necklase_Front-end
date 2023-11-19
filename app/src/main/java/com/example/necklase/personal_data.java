6fe472f4b35a04886e3eec7d23bb3b6f3acb15f9
Marco <marco1102004@gmail.com>
2023-11-18 20:51:28 -0600

    nueva funcion en navbar

--- app/src/main/java/com/example/necklase/personal_data.java
+++ app/src/main/java/com/example/necklase/personal_data.java
@@ -16,6 +16,7 @@ import android.widget.LinearLayout;
  */
 public class personal_data extends Fragment {
 
+    LinearLayout goBack;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,8 +61,17 @@ public class personal_data extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_personal_data, container, false);
+        View view = inflater.inflate(R.layout.fragment_personal_data, container, false);
 
+        goBack = view.findViewById(R.id.goBack);
 
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.frame, new personal_menu()).commit();
+            }
+        });
+
+        return view;
     }
 }
