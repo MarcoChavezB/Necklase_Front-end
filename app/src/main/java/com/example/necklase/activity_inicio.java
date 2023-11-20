908b78411b666e95918a600e3e7f344adb57e93a
anthony <tony20402014@gmail.com>
2023-11-19 19:16:15 -0600

    pantalla dispositivos e inicio

---
+++ app/src/main/java/com/example/necklase/activity_inicio.java
@@ -0,0 +1,14 @@
+package com.example.necklase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
