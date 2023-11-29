bb0379c0b209dd9134801105b66d16b585aeb61a
anthony <tony20402014@gmail.com>
2023-11-29 15:38:58 -0600

    shared prefenrences method

--- app/src/main/java/com/example/necklase/ViewModelToken/ViewModelTokenIns.java
+++ app/src/main/java/com/example/necklase/ViewModelToken/ViewModelTokenIns.java
@@ -2,24 +2,35 @@ package com.example.necklase.ViewModelToken;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.content.Context;
 import android.content.SharedPreferences;
-
-import androidx.lifecycle.AndroidViewModel;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.necklase.Model.Token.JwtUtils;
 
-public class ViewModelTokenIns extends AndroidViewModel {
+public class ViewModelTokenIns {
 
-    private String token;
-    private String userId;
-    private DecodedJWT decodedJWT;
-    public ViewModelTokenIns() {
-        super();
-        SharedPreferences prefs = getApplication().getApplicationContext().getSharedPreferences("loginPrefs", MODE_PRIVATE);
+    private ViewModelTokenIns viewModelTokenIns;
+    private static String token;
+    private static String userId;
+    private static DecodedJWT decodedJWT;
+    private ViewModelTokenIns() {
+
+    }
+
+    public static ViewModelTokenIns settoken(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("loginPrefs", MODE_PRIVATE);
         token = prefs.getString("token", null);
         decodedJWT = JwtUtils.decode(token);
         userId = decodedJWT.getSubject();
+        return null;
+    }
+
+    public ViewModelTokenIns getinstance() {
+        if (viewModelTokenIns == null) {
+            viewModelTokenIns = new ViewModelTokenIns();
+        }
+        return viewModelTokenIns;
     }
 
     public String getId() {
