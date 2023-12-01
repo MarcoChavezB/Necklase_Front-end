87f94b67ccee36a3e192bd13cd9213e5b927cd1a
anthony <tony20402014@gmail.com>
2023-12-01 15:32:21 -0600

    router de user con dispositivos o no, arreglado, + archivos de seleccionar device

--- app/src/main/java/com/example/necklase/MVVM/Interactors/LoginInteractor.java
+++ app/src/main/java/com/example/necklase/MVVM/Interactors/LoginInteractor.java
@@ -3,6 +3,8 @@ import static android.content.Context.MODE_PRIVATE;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.Toast;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.necklase.Model.IntanciasRetrofit.RetrofitApiModel;
@@ -13,6 +15,7 @@ import com.example.necklase.Model.Post.LoginManagment;
 import com.example.necklase.Model.Post.LoginPostModel;
 import com.example.necklase.Model.Token.JwtUtils;
 import com.example.necklase.Router.Router;
+import com.example.necklase.View.activity_bienvenida;
 import com.example.necklase.View.navbar;
 import com.example.necklase.View.selectDevice;
 import com.example.necklase.ViewModelToken.ViewModelTokenIns;
@@ -24,6 +27,7 @@ import retrofit2.Retrofit;
 
 public class LoginInteractor {
     private Context context;
+    private String id;
     public LoginInteractor(Context context){
            this.context = context;
     }
@@ -49,47 +53,16 @@ public class LoginInteractor {
                     editor.putString("token", response.body().getToken());
                     editor.apply();
 
-                    ViewModelTokenIns.getinstance();
-                    ViewModelTokenIns.settoken(context);
-
-                    Router.redirectTo(context, navbar.class);
-
+                    Intent intent = new Intent(context, activity_bienvenida.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
                 }else{
                     Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show();
                 }
-
             }
-
             @Override
             public void onFailure(Call<LoginPostModel> call, Throwable t) {
                 Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    public void checkDevices(String id){
-        RetrofitApiModelToken retro = new RetrofitApiModelToken();
-        Retrofit retrofit = retro.provideRetrofit();
-        CheckDevicesManagment checkDevicesManagment = new CheckDevicesManagment(retrofit);
-
-        checkDevicesManagment.getData(id, new Callback<CheckDevicesPostModel>() {
-            @Override
-            public void onResponse(Call<CheckDevicesPostModel> call, Response<CheckDevicesPostModel> response) {
-                if(response.isSuccessful()){
-                    SharedPreferences.Editor editor = context.getSharedPreferences("Personal", MODE_PRIVATE).edit();
-                    editor.putString("nDispositivos", response.body().getNumero());
-                    editor.apply();
-
-                    SharedPreferences dispositivos = context.getSharedPreferences("Personal", MODE_PRIVATE);
-                    String nDispositivos = dispositivos.getString("nDispositivos", null);
-
-                }else{
-                    Toast.makeText(context, "Error al ingresar dispositivos" + response.code(), Toast.LENGTH_SHORT).show();
-                }
-            }
-            @Override
-            public void onFailure(Call<CheckDevicesPostModel> call, Throwable t) {
-
             }
         });
     }
---
+++ app/src/main/java/com/example/necklase/View/activity_bienvenida.java
@@ -0,0 +1,97 @@
+package com.example.necklase.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.necklase.Model.IntanciasRetrofit.RetrofitApiModelToken;
+import com.example.necklase.Model.Post.CheckDevicesManagment;
+import com.example.necklase.Model.Post.CheckDevicesPostModel;
+import com.example.necklase.Model.Token.JwtUtils;
+import com.example.necklase.R;
+import com.example.necklase.Router.Router;
+import com.example.necklase.ViewModelToken.ViewModelTokenIns;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class activity_bienvenida extends AppCompatActivity {
+
+    private Button btn;
+    private String id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+
+        btn = findViewById(R.id.boton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewModelTokenIns.getinstance();
+                ViewModelTokenIns.settoken(activity_bienvenida.this);
+
+                SharedPreferences prefs = activity_bienvenida.this.getSharedPreferences("loginPrefs", MODE_PRIVATE);
+                String token = prefs.getString("token", null);
+                DecodedJWT decodedJWT = JwtUtils.decode(token);
+                DecodedJWT decode = JwtUtils.decode(token);
+
+                if (decode != null) {
+                    id = decode.getSubject();
+                    Log.e("ID bienvenida token", "El id es valido");
+                    checkDevices();
+                } else {
+                    Log.e("ID bienvenida token", "El token es nulo o inválido");
+                }
+
+            }
+        });
+    }
+    public void checkDevices(){
+        RetrofitApiModelToken retro = new RetrofitApiModelToken();
+        Retrofit retrofit = retro.provideRetrofit();
+        CheckDevicesManagment checkDevicesManagment = new CheckDevicesManagment(retrofit);
+        checkDevicesManagment.getData(this.id, new Callback<CheckDevicesPostModel>() {
+            @Override
+            public void onResponse(Call<CheckDevicesPostModel> call, Response<CheckDevicesPostModel> response) {
+                if(response.isSuccessful()){
+                    if(response.body().getNumero() == null){
+                        Toast.makeText(activity_bienvenida.this, "Error al cargar dispositivos", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if(Integer.parseInt(response.body().getNumero()) == 0){
+                        Router.redirectTo(activity_bienvenida.this, activity_anadir_dispositivo.class);
+                    }else{
+                        Router.redirectTo(activity_bienvenida.this, navbar.class);
+                    }
+                }else{
+                    Toast.makeText(activity_bienvenida.this, "Error al ingresar dispositivos" + response.code(), Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<CheckDevicesPostModel> call, Throwable t) {
+            }
+        });
+    }
+
+    private void CountDownTimer() {
+        new CountDownTimer(2000, 2000) {
+            public void onFinish() {
+            }
+            public void onTick(long millisUntilFinished) {
+            }
+        }.start();
+    }
+
+}
